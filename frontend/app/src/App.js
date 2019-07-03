845ee66fc9bd635e75877b218c03d664d15e031a
Hari Mohanraj <hari@shipt.com>
2019-07-03 00:22:20 -0700

    ripped out styling for session listing

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -8,12 +8,22 @@ import SessionListView from './SessionListView';
 import AnalyticsView from './AnalyticsView';
 import FilterView from './FilterView'
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
 
 import './App.css';
+
+
+// Mock data 
 
 var data = [
   {
@@ -31,77 +41,85 @@ var data = [
   }
 ];
 
-// var data = [
-//   {
-//     "avg_minutes_per_topic": 86.85714285714286, 
-//     "display_topic_name": "health", 
-//     "session_count": 7, 
-//     "total_minutes_per_topic": 608, 
-//     "true_topic_id": 3
-//   }, 
-//   {
-//     "avg_minutes_per_topic": 57.166666666666664, 
-//     "display_topic_name": "law", 
-//     "session_count": 6, 
-//     "total_minutes_per_topic": 343, 
-//     "true_topic_id": 8
-//   }
-// ]
+var sessions = [{"id":"109417","user_id":"400","site_id":"400","created":"1561418795","updated":"1561478971","cuepoints_updated":"1561478971","title":"06-24-19 Council Meeting Video","label":"06-24-19 Council Meeting Video","url":"https:\/\/Lakewood.open.media\/sessions\/109417","embed_url":"https:\/\/Lakewood.open.media\/embed\/sessions\/109417","video_url":"https:\/\/www.youtube.com\/watch?v=mzlptlkOqxU","video_id":"mzlptlkOqxU","video_image_url":"https:\/\/img.youtube.com\/vi\/mzlptlkOqxU\/0.jpg","archive_id":false,"date":"1561402800","documents":[{"id":"17636","file_id":"16675","url":"https:\/\/ompnetwork.s3-us-west-2.amazonaws.com\/sites\/400\/documents\/city-council-meeting-agenda-06-24-2019.pdf?LfBMpt28Tbqbg_zZjUsJZmrTARtEYaiP","label":null,"external_id":null,"session_id":"109417","session_title":"06-24-19 Council Meeting Video","user_id":"400","type":"Agenda","agenda_provider_id":null,"agenda_provider_agenda_id":null,"storage":"attachment","provider":"generic","type_id":"1"},{"id":"17637","file_id":"16677","url":"https:\/\/ompnetwork.s3-us-west-2.amazonaws.com\/sites\/400\/documents\/city-council-meeting-agenda-packet-06-24-2019.pdf?.WlrD.jr2Om2mNeANOkgRv8hIaGRsh29","label":null,"external_id":null,"session_id":"109417","session_title":"06-24-19 Council Meeting Video","user_id":"400","type":"Packet","agenda_provider_id":null,"agenda_provider_agenda_id":null,"storage":"attachment","provider":"generic","type_id":"5"}],"categories":[{"id":"311","user_id":"400","label":"Meetings","position":"0","livestream":"1","hidden":"0"}],"live_status":"0","minutes_status":"0"}]
 
-class App extends React.Component {
+
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  content: {
+    flexGrow: 1,
+    height: '100vh'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column'
+  }
+}));
+
+
+function App() {
+  
+  const classes = useStyles();
 
   // See this for state-change code:
   // ~ https://reactjs.org/docs/lifting-state-up.html
   // ~ https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html#fetching-external-data
+  // constructor(props) {
+  // 	super(props);
+  // 	this.state = { 
+  // 		sessions: [], 
+  // 		request_url: ''
+  // 	};
+  // }
+
+  // Add stateful stuff back in later! 
+  // componentDidMount() {
+  // 	// returns a promise, so don't do anything after and expect it to happen synchronously
+  // 	axios.get(`https://open.ompnetwork.org/api/site/400/sessions?limit=3&live=0`)
+  // 		.then((res) => {
+  // 			this.setState({
+  // 				sessions: res.data.results,
+  // 				request_url: 'https://open.ompnetwork.org/api/site/400/sessions?limit=10&live=0'
+  // 			});
+  // 		});
+  // }
+
+	return (
+
+    <div className={classes.root}> 
+
+      <AppBar position = "static">
+        <Toolbar>
+          <img src={logo} alt="logo" height="10%" width="10%"/>
+        </Toolbar>
+      </AppBar>
+
+
+      <Container className={classes.content} maxWidth="lg">
+        <Grid container spacing={3}>
+
+          <Grid item xs={12} md={5} lg={5}>
+            <Paper className={classes.paper}>
+              <SessionListView request_url="blah" sessions={sessions} />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={5} lg={5}>
+            <AnalyticsView data={data}/>
+          </Grid>
+
+          <Grid item xs={12} md={2} lg={2}>
+            <FilterView />
+          </Grid>
+
+        </Grid>
+      </Container>
+    </div>
+  );
   
-  constructor(props) {
-  	super(props);
-  	this.state = { 
-  		sessions: [], 
-  		request_url: ''
-  	};
-  }
-
-  componentDidMount() {
-  	// returns a promise, so don't do anything after and expect it to happen synchronously
-  	axios.get(`https://open.ompnetwork.org/api/site/400/sessions?limit=3&live=0`)
-  		.then((res) => {
-  			this.setState({
-  				sessions: res.data.results,
-  				request_url: 'https://open.ompnetwork.org/api/site/400/sessions?limit=10&live=0'
-  			});
-  		});
-  }
-
-  
-  render() {
-
-  	return (
-
-	    <div className = "Container"> 
-
-	      <AppBar position = "static">
-	        <Toolbar>
-	          <img src={logo} alt="logo" height="10%" width="10%"/>
-	        </Toolbar>
-	      </AppBar>
-      	<div className = "ContentView">
-  	      <SessionListView
-  	      	request_url = {this.state.request_url}
-  	      	sessions = {this.state.sessions}
-  	      />
-
-          <AnalyticsView
-            data= {data}
-          />
-
-          <FilterView
-          />
-        </div>
-	    </div>
-
-	);
-  }
 }
 
 export default App;
--- frontend/app/src/SessionListView.js
+++ frontend/app/src/SessionListView.js
@@ -10,21 +10,29 @@ import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 
 
-class SessionListView extends React.Component {
+function SessionListView(props) {
 
-	constructor(props) {
-		// props 
-		// ~ a list of sessions
-		super(props);
-		this.state = {
-			request_url: props.request_url || '',
-			sessions: props.sessions || []
-		}
-		console.log("Session List View Constructor::")
-		console.log(this.state)
-	}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		request_url: props.request_url || '',
+	// 		sessions: props.sessions || []
+	// 	}
+	// }
 
-	transformSessionsToListItems(sessions) {
+	// static getDerivedStateFromProps(nextProps, prevState) {
+	// 	if(nextProps.request_url !== prevState.request_url) {
+	// 		return { 
+	// 			request_url: nextProps.request_url,
+	// 			sessions: nextProps.sessions
+	// 		};
+	// 	} else {
+	// 		return null;
+	// 	}
+
+	// }
+
+	const transformSessionsToListItems = (sessions) => {
 		if(sessions.length > 0) {
 			const listItems = sessions.map((session) => 
 				<ListItem>
@@ -37,31 +45,12 @@ class SessionListView extends React.Component {
 			return null;
 		}
 	}
-
-	static getDerivedStateFromProps(nextProps, prevState) {
-		if(nextProps.request_url !== prevState.request_url) {
-			console.log('getDerivedStateFromProps::')
-			console.log('nextProps', nextProps)
-			console.log('prevState', prevState)
-			return { 
-				request_url: nextProps.request_url,
-				sessions: nextProps.sessions
-			};
-		} else {
-			return null;
-		}
-
-	}
-
-	render() {
-		return (
-			<div className="SessionList">
-				<Paper elevation={1}>
-					<List>{this.transformSessionsToListItems(this.state.sessions)}</List>
-				</Paper>
-			</div>
-		);
-	}
+	
+	return (
+		<div className="SessionList">
+			<List>{transformSessionsToListItems(props.sessions)}</List>
+		</div>
+	);
 }
 
 export default SessionListView;
