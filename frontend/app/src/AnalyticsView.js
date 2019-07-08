21a5a1e5b6eda94885836c9008132f4a07437281
Hari Mohanraj <hari@shipt.com>
2019-07-07 21:56:38 -0700

    fixing charts on analytics view and added dummy data

--- frontend/app/src/AnalyticsView.js
+++ frontend/app/src/AnalyticsView.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {ResponsivePie} from '@nivo/pie'
+import {
+  BarChart, Bar, Cell, XAxis,
+  YAxis, CartesianGrid, Tooltip, Legend,
+  PieChart, Pie, ResponsiveContainer
+} from 'recharts';
+
 
 import { GavelRounded, RecordVoiceOver } from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+
+const data = [
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
+
+const data2 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 
 const useStyles = makeStyles(theme => ({
@@ -35,8 +72,64 @@ const useStyles = makeStyles(theme => ({
   	icon: {
   		paddingTop: theme.spacing(1),
   		fontSize: 32
+  	},
+  	minutesPerTopicByTime: {
+  		height: 240,
+  		paddingTop: theme.spacing(2),
+  		paddingBottom: theme.spacing(6)
+  	},
+  	minutesPerTopic: {
+  		height: 240,
+  		paddingTop: theme.spacing(2),
+  		paddingBottom: theme.spacing(2)
   	}
 }));
+
+function MinutesPerTopicByTime() {
+	return (
+		<ResponsiveContainer>
+	      <BarChart
+	        data={data}
+	        margin={{
+	          top: 10, right: 10, left: 10, bottom: 0,
+	        }}
+	      >
+	        <CartesianGrid strokeDasharray="3 3" />
+	        <XAxis dataKey="name" />
+	        <YAxis />
+	        <Tooltip />
+	        <Legend />
+	        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+	        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+	      </BarChart>
+	    </ResponsiveContainer>
+    );
+}
+
+function MinutesPerTopic() {
+	return (
+		<ResponsiveContainer>
+			<PieChart>
+		        <Pie
+		          data={data2}
+		          cx={'50%'}
+		          cy={'50%'}
+		          innerRadius={'60%'}
+		          outerRadius={'80%'}
+		          fill="#8884d8"
+		          paddingAngle={5}
+		          dataKey="value"
+		        >
+		          {
+		            data2.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+		          }
+		        </Pie>
+		        <Legend layout='vertical' align='right' verticalAlign='middle' />
+		  	</PieChart>
+		 </ResponsiveContainer>
+	);
+}
+
 
 
 function AnalyticsView(props) {
@@ -54,7 +147,7 @@ function AnalyticsView(props) {
 					<div className={classes.metricData}>
 						{/*Replace with prop*/}
 						<Typography variant="h5">1000</Typography>
-						<Typography variant="caption">Metric explanation 1</Typography>
+						<Typography variant="caption">Sessions</Typography>
 					</div>
 				</div>
 				<div className={classes.metric}>
@@ -62,37 +155,17 @@ function AnalyticsView(props) {
 					<div className={classes.metricData}>
 						{/*Replace with prop*/}
 						<Typography variant="h5">1234</Typography>
-						<Typography variant="caption">Metric explanation 2</Typography>
+						<Typography variant="caption">Minutes of Conversation</Typography>
 					</div>
 				</div>
 			</div>
-
-
-			<div className="ChartContainer">
-				<ResponsivePie
-			        data={props.data}
-			        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-			        innerRadius={0.5}
-			        padAngle={0.7}
-			        cornerRadius={3}
-			        colors={{ scheme: 'set1' }}
-			        borderWidth={1}
-			        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
-			        radialLabel='label'
-			        radialLabelsSkipAngle={10}
-			        radialLabelsTextXOffset={6}
-			        radialLabelsTextColor="#333333"
-			        radialLabelsLinkOffset={0}
-			        radialLabelsLinkDiagonalLength={16}
-			        radialLabelsLinkHorizontalLength={24}
-			        radialLabelsLinkStrokeWidth={1}
-			        radialLabelsLinkColor={{ from: 'color' }}
-			        slicesLabelsSkipAngle={10}
-			        slicesLabelsTextColor="#333333"
-			        animate={true}
-			        motionStiffness={90}
-			        motionDamping={15}
-			    />
+			<div className={classes.minutesPerTopicByTime}>
+				<Typography variant="h6">Minutes Per Topic Over Time</Typography>
+				<MinutesPerTopicByTime />
+			</div>
+			<div className={classes.minutesPerTopic}>
+				<Typography variant="h6">Minutes Per Topic</Typography>
+				<MinutesPerTopic />
 			</div>
 		</div>
 		
